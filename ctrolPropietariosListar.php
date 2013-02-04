ae611ab98fa039b08d0534d8b5297b9838e00555
Laura <Lauradiezc1805@gmail.com>
2013-02-04 10:05:05 -0500

    segundo commit

--- ctrolPropietariosListar.php
+++ ctrolPropietariosListar.php
@@ -4,11 +4,6 @@
 
 
 
-	$method = $_POST['mathod'];
-	$o = new Ctrl_Propietarios();
-	$o->$method();
-
-
 	class Ctrl_Propietarios{
 		private $modelUser;
 
@@ -17,19 +12,7 @@
 			$this->modelUser = new clsPropietarios();
 		}
 
-		/**
-		* Este metodo retorna los usuarios.
-		*	@author Laura
-		*	@return array
-		*/
-		public function getPropietarios()
-		{
-			$res = $this->modelUser->getAllPropietarios();
-			$Propietarios = array();
-			while($r = $res->fetch_array())
-				$Propietarios[] = $r;
-			echo json_encode($Propietarios);
-		}
+
 
 	}
 ?>
